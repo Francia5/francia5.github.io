bf5de16c27de336a702048187332e9e727b962ce
francia5 <francia.va.or@gmail.com>
2018-08-30 14:05:58 -0500

    se agregan cambios para la visualización de las secciones

--- name.js
+++ name.js
@@ -10,3 +10,44 @@ $(function () {
         }
     });
 });
+
+
+let spans = document.querySelectorAll('.word span');
+spans.forEach((span, idx) => {
+    span.addEventListener('click', (e) => {
+        e.target.classList.add('active');
+    });
+    span.addEventListener('animationend', (e) => {
+        e.target.classList.remove('active');
+    });
+
+    // Initial animation
+    setTimeout(() => {
+        span.classList.add('active');
+    }, 750 * (idx + 1))
+});
+
+// $(function () {
+//     var nav = $(".juan");
+//     $(window).scroll(function () {
+//         var scroll = $(window).scrollTop();
+//         nav
+
+//         if (scroll) {
+//             nav.removeClass(".juan");
+//             nav.addClass(".change");
+//         } 
+//     });
+// });
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600 && window.scrollY < 1100) {
+            $('.navscroll').addClass("change");
+        } else if (window.scrollY >= 1100 && window.scrollY <= 1200) {
+            $(".navscroll").removeClass("change");
+        }  else {
+            $(".navscroll").removeClass("change");
+        }
+    });
+});
